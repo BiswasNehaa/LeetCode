eb1e7843f771a6dcba788786a9dc2b56e7002ad9
Neha Biswas <neha12345biswas@gmail.com>
2026-07-30 23:17:21 +0530

    Time: 3 ms (73.16%) | Memory: 12.5 MB (38.01%) - LeetSync

---
+++ 20-valid-parentheses/valid-parentheses.py
@@ -0,0 +1,26 @@
+class Solution(object):
+    def isValid(self, s):
+        """
+        :type s: str
+        :rtype: bool
+        """
+        if len(s)%2 != 0:
+            return False
+        
+        stack=[]
+        for bracket in s:
+            if bracket=="(" or bracket=="{" or bracket=="[" :
+                stack.append(bracket)
+            else:
+                if len(stack)==0:
+                    return False
+                ch=stack.pop()
+                if((bracket==")" and ch=="(") or
+                    (bracket=="}" and ch=="{") or
+                    (bracket=="]" and ch=="[") 
+                ):
+                    continue
+                else:
+                    return False
+                
+        return len(stack)==0
