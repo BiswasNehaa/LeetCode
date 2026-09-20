e4e5b614c8dc79af41dbf5adba030ea622ebd58a
Neha Biswas <neha12345biswas@gmail.com>
2026-09-20 21:02:00 +0530

    Time: 1793 ms (40.90%) | Memory: 13.3 MB (17.94%) - LeetSync

--- 1-two-sum/two-sum.py
+++ 1-two-sum/two-sum.py
@@ -5,9 +5,12 @@ class Solution(object):
         :type target: int
         :rtype: List[int]
         """
-        hash_map={}
-        for i in range (len(nums)):
-            rem=target-nums[i]
-            if rem  in  hash_map:
-                return [hash_map[rem],i]
-            hash_map[nums[i]]=i
+        
+        for i in range(len(nums)):
+            for j in range (i+1,len(nums)):
+                if nums[i]+nums[j]==target:
+                    return [i,j]
+                
+        
+        
+
